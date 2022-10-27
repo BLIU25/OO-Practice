5456b55c8908eb36250c75cd747c70a9c6d74c7d
Bingqi <BLiu25@slb.com>
2022-10-27 14:02:34 +0800

    feat: add driver.

---
+++ OOPractice/Driver.cs
@@ -0,0 +1,19 @@
+﻿using OOPractice;
+
+namespace OOPracticeTest
+{
+    public class Driver
+    {
+        private Vehicle vehicle;
+
+        public Driver(Vehicle vehicle)
+        {
+            this.vehicle = vehicle;
+        }
+
+        public string SpeedUp()
+        {
+            return vehicle.SpeedUp();
+        }
+    }
+}
--- OOPracticeTest/CarTest.cs
+++ OOPracticeTest/CarTest.cs
@@ -33,5 +33,33 @@ namespace OOPracticeTest
             //then
             Assert.Equal("Big truck: speed up 10km/h", msg);
         }
+
+        [Fact]
+        public void Should_return_msg_when_speed_up_given_driver_car_name_speed()
+        {
+            //given
+            Vehicle vehicle = new Vehicle("Cool car", 30);
+            Driver driver = new Driver(vehicle);
+
+            //when
+            string msg = driver.SpeedUp();
+
+            //then
+            Assert.Equal("Cool car: speed up 30km/h", msg);
+        }
+
+        [Fact]
+        public void Should_return_msg_when_speed_up_given_driver_truck_name_speed()
+        {
+            //given
+            Vehicle vehicle = new Vehicle("Big truck", 10);
+            Driver driver = new Driver(vehicle);
+
+            //when
+            string msg = driver.SpeedUp();
+
+            //then
+            Assert.Equal("Big truck: speed up 10km/h", msg);
+        }
     }
 }
