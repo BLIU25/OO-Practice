1725ca470dbd7d93e246c171e680703bc96416ee
Bingqi <BLiu25@slb.com>
2022-10-27 11:48:35 +0800

    feat: add truck msg when speed up

---
+++ OOPractice/Truck.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using OOPractice;
+
+namespace OOPracticeTest
+{
+    public class Truck : Car
+    {
+        public Truck(string name, int speed) : base(name, speed)
+        {
+        }
+    }
+}
--- OOPracticeTest/CarTest.cs
+++ OOPracticeTest/CarTest.cs
@@ -3,6 +3,7 @@ using System.Runtime.CompilerServices;
 namespace OOPracticeTest
 {
     using OOPractice;
+    using System.Diagnostics;
     using Xunit;
 
     public class CarTest
@@ -18,6 +19,19 @@ namespace OOPracticeTest
 
             //then
             Assert.Equal("Cool car: speed up 30km/h", msg);
+        }
+
+        [Fact]
+        public void Should_return_msg_when_speed_up_given_truck_name_speed()
+        {
+            //given
+            Truck truck = new Truck("Big truck", 10);
+
+            //when
+            string msg = truck.SpeedUp();
+
+            //then
+            Assert.Equal("Big truck: speed up 10km/h", msg);
         }
     }
 }
